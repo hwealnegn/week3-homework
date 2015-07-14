79e3c76acd9e1f8b8d90dc401a13058ca16f3557
Helen Wang <hwealnegn@gmail.com>
2015-07-13 21:28:15 -0500

    Completed edit functionality and added descriptions for seed places

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Place.delete_all
 
-Place.create(:title => "National Palace Museum", :photo_url => "https://upload.wikimedia.org/wikipedia/commons/b/b4/NationalPalace_MuseumFrontView.jpg", :admission_price => 800, :description => "This is a description")
-Place.create(:title => "Taipei 101", :photo_url => "https://upload.wikimedia.org/wikipedia/commons/c/c9/Taipei101.portrait.altonthompson.jpg", :admission_price => 1600, :description => "This is a description")
-Place.create(:title => "Chiang Kai-Shek Memorial Hall", :photo_url => "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Chiang_Kai-shek_memorial_amk.jpg/1920px-Chiang_Kai-shek_memorial_amk.jpg", :admission_price => 0, :description => "This is a description")
-Place.create(:title => "Sun Yat-Sen Memorial Hall", :photo_url => "https://upload.wikimedia.org/wikipedia/commons/c/ce/SYS_Memorial_from_sky.jpg", :admission_price => 0, :description => "This is a description")
-Place.create(:title => "Taipei Fine Arts Museum", :photo_url => "https://upload.wikimedia.org/wikipedia/commons/6/64/Taipei-fine-arts-museum.jpg", :admission_price => 100, :description => "This is a description")
+Place.create(:title => "National Palace Museum", :photo_url => "https://upload.wikimedia.org/wikipedia/commons/b/b4/NationalPalace_MuseumFrontView.jpg", :admission_price => 800, :description => "An antique museum with a collection of ancient Chinese imperial artifacts and artworks")
+Place.create(:title => "Taipei 101", :photo_url => "https://upload.wikimedia.org/wikipedia/commons/c/c9/Taipei101.portrait.altonthompson.jpg", :admission_price => 1600, :description => "A landmark skyscraper that held the title of world's tallest building in 2004-2010")
+Place.create(:title => "Chiang Kai-Shek Memorial Hall", :photo_url => "https://upload.wikimedia.org/wikipedia/commons/d/d2/Chiang_Kai-shek_memorial_amk.jpg", :admission_price => 0, :description => "A national monument erected in memory of the former President of the Republic of China")
+Place.create(:title => "Sun Yat-Sen Memorial Hall", :photo_url => "https://upload.wikimedia.org/wikipedia/commons/c/ce/SYS_Memorial_from_sky.jpg", :admission_price => 0, :description => "A memorial for the National Father of the Republic of China")
+Place.create(:title => "Taipei Fine Arts Museum", :photo_url => "https://upload.wikimedia.org/wikipedia/commons/6/64/Taipei-fine-arts-museum.jpg", :admission_price => 100, :description => "The first museum in Taiwan built for contemporary art exhibitions")
